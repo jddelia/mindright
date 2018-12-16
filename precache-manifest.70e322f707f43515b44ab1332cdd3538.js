eea1cbdba976821cf7d3df14ec0e6a0ecdccad8f
jddelia <jddelia123@gmail.com>
2018-12-16 16:09:30 -0500

    Updates

---
+++ precache-manifest.70e322f707f43515b44ab1332cdd3538.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4482e7072b0c7214cc1",
+    "url": "/mindright/static/js/runtime~main.b4482e70.js"
+  },
+  {
+    "revision": "753501ce89ddb145884b",
+    "url": "/mindright/static/js/main.753501ce.chunk.js"
+  },
+  {
+    "revision": "1840f6304ee3af639dd7",
+    "url": "/mindright/static/js/1.1840f630.chunk.js"
+  },
+  {
+    "revision": "753501ce89ddb145884b",
+    "url": "/mindright/static/css/main.7009a94a.chunk.css"
+  },
+  {
+    "revision": "70e908978e9da7557e5f75a2cb819cdf",
+    "url": "/mindright/index.html"
+  }
+];
