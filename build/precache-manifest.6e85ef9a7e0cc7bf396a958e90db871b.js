ac288914fbe25e517e45217b99e5de683dfaf43c
jddelia <jddelia123@gmail.com>
2019-01-04 11:44:22 -0500

    Prep for heroku deploy

---
+++ build/precache-manifest.6e85ef9a7e0cc7bf396a958e90db871b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "297c1f15de9bc862bbf6",
+    "url": "/static/css/main.ef43cd3d.chunk.css"
+  },
+  {
+    "revision": "297c1f15de9bc862bbf6",
+    "url": "/static/js/main.297c1f15.chunk.js"
+  },
+  {
+    "revision": "2965236758e665c473d3",
+    "url": "/static/js/1.29652367.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2104abd9643ad8f84f26b366c5f0a4b",
+    "url": "/index.html"
+  }
+];
